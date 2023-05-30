bfa998a7b43e7cb54f2cee6c0fe810723ce4fd36
normalset <pikminbianco@gmail.com>
2023-05-30 23:19:25 +0200

    updt

--- Projects/badApple/testBadApple.js
+++
@@ -1,8 +0,0 @@
-aalib.read.video.fromVideoElement(document.querySelector("video"))
-  .pipe(aalib.aa({ width: 165, height: 68 }))
-  .pipe(aalib.render.canvas({
-      width: 480,
-      height: 360,
-      el: document.querySelector("#video-scene")
-  }))
-  .end();
