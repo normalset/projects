ae3c9eb4e31e316ca22fe7d351beda4d2d201ae9
normalset <pikminbianco@gmail.com>
2023-05-30 19:53:46 +0200

    updt

--- Projects/pygameTest/test.py
+++ Projects/pygameTest/test.py
@@ -8,7 +8,7 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('./font/Pixeltype.ttf' , 50)
 
-
+game_active = True
 width = 800
 height = 400
 screen = pygame.display.set_mode((width , height))
@@ -32,36 +32,41 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos) : print('mouse collision detected')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-            player_gravity = -20
-        if event.type == pygame.MOUSEBUTTONDOWN and player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-            player_gravity -= 20
-            
+        if game_active: 
+            # if event.type == pygame.MOUSEMOTION:
+            #     if player_rect.collidepoint(event.pos) : print('mouse collision detected')
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity -= 20
+        else: 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE : 
+                game_active = True
+                snail_rect.left = 800
 
-    
-            
-    screen.blit(ground_surf, (0 , 300))
-    screen.blit(sky_surf, (0,0))
+    if game_active :           
+        screen.blit(ground_surf, (0 , 300))
+        screen.blit(sky_surf, (0,0))
 
-    #rect(surface , color, thing to draw , line width , border radius, )
-    pygame.draw.rect(screen , '#c0e8ec' , score_rect , 10 , 10)
-    pygame.draw.rect(screen , '#c0e8ec' , score_rect)
-    screen.blit(score_surf , score_rect)
+        #rect(surface , color, thing to draw , line width , border radius, )
+        pygame.draw.rect(screen , '#c0e8ec' , score_rect , 10 , 10)
+        pygame.draw.rect(screen , '#c0e8ec' , score_rect)
+        screen.blit(score_surf , score_rect)
 
-    screen.blit(snail_surf, snail_rect)
-    snail_rect.x -= 5
-    if snail_rect.right <= 0 : snail_rect.left = 800
-    screen.blit(player_surf, player_rect)
-    
-    # player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300 : player_rect.bottom = 300
-    screen.blit(player_surf , player_rect)
+        screen.blit(snail_surf, snail_rect)
+        snail_rect.x -= 5
+        if snail_rect.right <= 0 : snail_rect.left = 800
+        screen.blit(player_surf, player_rect)
+        
+        # player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300 : player_rect.bottom = 300
+        screen.blit(player_surf , player_rect)
 
-    #collisions
+        #collisions
+        if player_rect.colliderect(snail_rect) : game_active = False
+    else: screen.fill('Black')
 
     pygame.display.update()
     clock.tick(60)
